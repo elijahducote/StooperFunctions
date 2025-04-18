28c6c874338fcc5818e1b611d920da8b9d79c3cf
Elijah Ducote <validpupflower@gmail.com>
2025-04-18 08:51:23 -0500

    Changes

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ import {
 } from "./functions/djev/ntry.js";
 const app = new Hono();
 
+app.post("/create-intent", wrapper(createIntent,"HONO"));
+app.post("/deliver", wrapper(deliver,"HONO"));
 app.get("/flyer-update", wrapper(flyerUpdate,"HONO"));
+app.get("/important", wrapper(important,"HONO"));
+app.post("/message", wrapper(message,"HONO"));
+app.post("/request", wrapper(request,"HONO"));
+app.post("/resend", wrapper(resend,"HONO"));
+app.post("/subscribe", wrapper(subscribe,"HONO"));
 
 Deno.serve(app.fetch);

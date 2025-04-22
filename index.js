104b670e52ffabc8d07d5c602be887a1af2dbd00
ElijahDucote <validpupflower@gmail.com>
2025-04-22 04:01:21 -0500

    Changes

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ import {
 } from "./functions/djev/ntry.js";
 const app = new Hono();
 
-Deno.cron("Run every 24 hours", "1 */24 * * *", flyerUpdate);
+Deno.cron("Run every 24 hours", "0 0 * * *", {
+  backoffSchedule: [500, 500, 500, 500, 500]
+}, flyerUpdate);
 
 app.post("/create-intent", wrapper(createIntent,"HONO"));
 app.post("/deliver", wrapper(deliver,"HONO"));
